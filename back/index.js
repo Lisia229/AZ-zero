3d01674592d486625fd7383dd7ca87cadeeaa2dd
Lisia229 <h200022923@gmail.com>
2023-02-22 08:50:28 +0800

    後台頁面修改

--- back/index.js
+++ back/index.js
@@ -1,7 +1,8 @@
 import 'dotenv/config'
 import express from 'express'
-import mongoose from 'mongoose'
+import mongoose, { set } from 'mongoose'
 import cors from 'cors'
+import https from 'https'
 import userRoute from './routes/users.js'
 import exhibitionsRoute from './routes/exhibitions.js'
 import rentalsRoute from './routes/rentals.js'
@@ -54,9 +55,9 @@ app.use('/products', productsRoute)
 app.use('/orders', orderRoute)
 app.use('/rentalorders', rentalordersRoute)
 
-app.get('/', (req, res) => {
-  res.status(200).json({ success: true, message: '' })
-})
+// app.get('/', (req, res) => {
+//   res.status(200).json({ success: true, message: '' })
+// })
 
 app.all('*', (req, res) => {
   res.status(404).json({ success: false, message: '找不到' })
@@ -65,3 +66,9 @@ app.all('*', (req, res) => {
 app.listen(process.env.PORT || 4000, () => {
   console.log('伺服器啟動')
 })
+
+if (process.env.RENDER) {
+  setInterval(() => {
+    https.get(process.env.RENDER)
+  }, 1000 * 60 * 5)
+}
