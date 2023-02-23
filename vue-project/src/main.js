d58ab801ae8df3775b63eae612a30075f3988320
Lisia229 <h200022923@gmail.com>
2023-02-23 15:41:26 +0800

    亂改

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,16 +1,16 @@
 import App from './App.vue'
 
 import { createApp } from 'vue'
-
 import router from './router'
 
 import pinia from './plugins/pinia'
-
+import SfVueAnimatedCursor from 'sf-vue-animated-cursor'
 import './css/style.css'
 import { useUserStore } from '@/stores/users'
 ;(async () => {
   const app = createApp(App)
   app.use(pinia)
+  app.use(SfVueAnimatedCursor, { basic: false })
   await useUserStore().getUser()
   app.use(router)
   app.mount('#app')
